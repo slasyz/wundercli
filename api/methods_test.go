451a5b27844f95be46e2d002d134b30faf0c2f20
Vyacheslav Syrovatsky <slasyz@gmail.com>
2016-02-03 03:40:45 +0300

    Added parsing test.

---
+++ api/methods_test.go
@@ -0,0 +1,20 @@
+package api
+
+import (
+	"testing"
+	"fmt"
+	"time"
+)
+
+func TestParseDate(t *testing.T) {
+	expectedTaskText := "Do something"
+	expectedDate := fmt.Sprintf("%d-05-12", time.Now().Year())
+
+	taskText, date := parseDate("Do something     May 12")
+	if taskText != expectedTaskText {
+		t.Fatalf("Wrong task text: expected \"%s\", got \"%s\"", expectedTaskText, taskText)
+	}
+	if date != expectedDate {
+		t.Fatalf("Wrong month: expected %s, got %s", expectedDate, date)
+	}
+}
